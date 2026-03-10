ab19c04894e637d1a89a6b670bde6b12886ab18b
brad4edat <brad4edatrazlog@mail.bg>
2026-03-10 16:18:01 +0200

    Initial commit of new project

---
+++ include/LGFX_Config.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <LovyanGFX.hpp>
+
+class LGFX : public lgfx::LGFX_Device {
+ private:
+  lgfx::Panel_ILI9341 _panel_instance;
+  lgfx::Bus_SPI _bus_instance;
+  lgfx::Touch_XPT2046 _touch_instance;
+
+ public:
+  LGFX(void) {
+    {
+      auto cfg = _bus_instance.config();
+      cfg.spi_host = VSPI_HOST;
+      cfg.spi_mode = 0;
+      cfg.freq_write = 40000000;
+      cfg.freq_read = 16000000;
+      cfg.spi_3wire = false;
+      cfg.use_lock = true;
+      cfg.dma_channel = 1;
+      cfg.pin_sclk = 18;
+      cfg.pin_mosi = 23;
+      cfg.pin_miso = 19;
+      cfg.pin_dc = 16;
+      _bus_instance.config(cfg);
+      _panel_instance.setBus(&_bus_instance);
+    }
+
+    {
+      auto cfg = _panel_instance.config();
+      cfg.pin_cs = 5;
+      cfg.pin_rst = 4;
+      cfg.pin_busy = -1;
+      cfg.panel_width = 240;
+      cfg.panel_height = 320;
+      cfg.offset_x = 0;
+      cfg.offset_y = 0;
+      cfg.offset_rotation = 0;
+      cfg.dummy_read_pixel = 8;
+      cfg.dummy_read_bits = 1;
+      cfg.readable = true;
+      cfg.invert = false;
+      cfg.rgb_order = false;
+      cfg.dlen_16bit = false;
+      cfg.bus_shared = true;
+      _panel_instance.config(cfg);
+    }
+
+    {
+      auto cfg = _touch_instance.config();
+      cfg.x_min = 0;
+      cfg.x_max = 239;
+      cfg.y_min = 0;
+      cfg.y_max = 319;
+      cfg.pin_int = 27;
+      cfg.bus_shared = true;
+      cfg.offset_rotation = 0;
+      cfg.spi_host = VSPI_HOST;
+      cfg.freq = 1000000;
+      cfg.pin_sclk = 18;
+      cfg.pin_mosi = 23;
+      cfg.pin_miso = 19;
+      cfg.pin_cs = 14;
+      _touch_instance.config(cfg);
+      _panel_instance.setTouch(&_touch_instance);
+    }
+
+    setPanel(&_panel_instance);
+  }
+};
---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+#include <LovyanGFX.hpp>
+
+#include "LGFX_Config.h"
+
+LGFX tft;
+
+namespace ui {
+constexpr int kScreenW = 320;
+constexpr int kScreenH = 240;
+
+void drawBatteryIcon(int x, int y, int bodyW, int bodyH, int color) {
+  // Battery body (horizontal icon) with terminal nub on the right.
+  const int stroke = 2;
+  const int nubW = 4;
+  const int nubH = bodyH / 2;
+  const int nubY = y + (bodyH - nubH) / 2;
+
+  tft.drawRect(x, y, bodyW, bodyH, color);
+  tft.drawRect(x + stroke, y + stroke, bodyW - (2 * stroke), bodyH - (2 * stroke), color);
+  tft.fillRect(x + bodyW, nubY, nubW, nubH, color);
+}
+
+void drawStartupScreen() {
+  tft.fillScreen(TFT_BLACK);
+
+  tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  tft.setTextDatum(top_left);
+  tft.setFont(&fonts::efontCN_16);
+  tft.drawString("Кубол Балансьор", 8, 8);
+
+  const int batteryW = 26;
+  const int batteryH = 12;
+  const int margin = 8;
+  const int batteryX = kScreenW - batteryW - 4 - margin;
+  const int batteryY = 8;
+  drawBatteryIcon(batteryX, batteryY, batteryW, batteryH, TFT_WHITE);
+}
+}  // namespace ui
+
+void setup() {
+  Serial.begin(115200);
+
+  tft.init();
+  tft.setRotation(1);  // Landscape: 320x240
+
+  ui::drawStartupScreen();
+}
+
+void loop() {
+  delay(10);
+}
